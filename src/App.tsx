2fd25607d4bdb3f0a708f3e8c232ad3720cc7485
RafaelGomes <rafael.silva@readi.com.br>
2024-02-12 11:06:06 -0300

    refactor: reorganiza componentes default para facilitar o desenvolvimento

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import Dashboard from './components/Dashboard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import Dashboard from './pages/Dashboard'
+import Consumers from './pages/Consumers';
+import { useState } from 'react';
+import { Box } from '@mui/material';
+import { Appbar } from './components/Appbar';
+import { Drawer } from './components/Drawer';
 
 function App() {
+  const defaultTheme = createTheme();
+
+  const [open, setOpen] = useState(true);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
-      <Dashboard />
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Box sx={{ display: 'flex' }}>
+          <Appbar open={open} toggleFunction={toggleDrawer} />
+          <CssBaseline />
+          <Drawer open={open} toggleFunction={toggleDrawer} />
+
+          <Routes>
+            <Route path='/' element={<  Dashboard />} />
+            <Route path='/consumers' element={<Consumers />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+
+    </ThemeProvider>
   );
 }
 
